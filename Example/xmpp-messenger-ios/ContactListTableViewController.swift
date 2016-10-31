454234686876f21ef253c8d3f976b6bc4097a836
Haris <highervisibility@Highers-MacBook-Pro.local>
2016-10-31 16:40:44 +0500

    Onwords edits by Sir, Hairs

--- Example/xmpp-messenger-ios/ContactListTableViewController.swift
+++ Example/xmpp-messenger-ios/ContactListTableViewController.swift
@@ -11,52 +11,50 @@ import XMPPFramework
 import xmpp_messenger_ios
 
 protocol ContactPickerDelegate{
-	func didSelectContact(recipient: XMPPUserCoreDataStorageObject)
+    func didSelectContact(recipient: XMPPUserCoreDataStorageObject)
 }
 
 class ContactListTableViewController: UITableViewController, OneRosterDelegate, UIGestureRecognizerDelegate {
-	
-	var delegate:ContactPickerDelegate?
-	
-	// Mark : Life Cycle
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
+    
+    var delegate:ContactPickerDelegate?
+    
+    // Mark : Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
         let longPressGesture:UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(ContactListTableViewController.handleLongPress(_:)))
         longPressGesture.minimumPressDuration = 1.0 // 1 second press
         longPressGesture.allowableMovement = 15 // 15 points
         longPressGesture.delegate = self
         self.tableView.addGestureRecognizer(longPressGesture)
         
-		OneRoster.sharedInstance.delegate = self
-	}
-	
-	override func viewWillAppear(animated: Bool) {
-		super.viewWillAppear(animated)
-		
-		if OneChat.sharedInstance.isConnected() {
-			navigationItem.title = "Contacts"
-		}
+        OneRoster.sharedInstance.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if OneChat.sharedInstance.isConnected() {
+            navigationItem.title = "Contacts"
+        }
         self.tableView.rowHeight = 75
-	}
+    }
     
-	override func viewWillDisappear(animated: Bool) {
-		super.viewWillDisappear(animated)
-		OneRoster.sharedInstance.delegate = nil
-	}
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        OneRoster.sharedInstance.delegate = nil
+    }
     
-	
-	func oneRosterContentChanged(controller: NSFetchedResultsController) {
-		tableView.reloadData()
-	}
+    
+    func oneRosterContentChanged(controller: NSFetchedResultsController) {
+        tableView.reloadData()
+    }
     
     
     // Mark: Gesture Handler
     
     func handleLongPress(longPressGesture:UILongPressGestureRecognizer) {
-        
-        
         
         let p = longPressGesture.locationInView(self.tableView)
         let indexPath = self.tableView.indexPathForRowAtPoint(p)
@@ -72,67 +70,65 @@ class ContactListTableViewController: UITableViewController, OneRosterDelegate, 
         }
         
     }
-	
-	// Mark: UITableView Datasources
-	
-	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		let sections: NSArray? =  OneRoster.buddyList.sections
-		
-		if section < sections!.count {
-			let sectionInfo: AnyObject = sections![section]
-			
-			return sectionInfo.numberOfObjects
-		}
-		
-		return 0;
-	}
-	
-	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    
+    // Mark: UITableView Datasources
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sections: NSArray? =  OneRoster.buddyList.sections
         
-		return OneRoster.buddyList.sections!.count
-	}
-	
-	// Mark: UITableView Delegates
-	
-//	override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-//		let sections: NSArray? = OneRoster.sharedInstance.fetchedResultsController()!.sections
-//        if section < sections!.count {
-//			let sectionInfo: AnyObject = sections![section]
-//			let tmpSection: Int = Int(sectionInfo.name)!
-//			
-//			switch (tmpSection) {
-//			case 0 :
-//				return "Online"
-//				
-//			case 1 :
-//				return "Away"
-//				
-//			default :
-//				return "Offline"
-//				
-//			}
-//		}
-//		
-//		return ""
-//	}
-	
-	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-		_ = OneRoster.userFromRosterAtIndexPath(indexPath: indexPath)
-		
-		delegate?.didSelectContact(OneRoster.userFromRosterAtIndexPath(indexPath: indexPath))
+        if section < sections!.count {
+            let sectionInfo: AnyObject = sections![section]
+            
+            return sectionInfo.numberOfObjects
+        }
         
-//		close(self)
+        return 0;
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
-        print("didSelectRowAtIndexPAth")
-	}
-	
-	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return OneRoster.buddyList.sections!.count
+    }
+    
+    // Mark: UITableView Delegates
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let sections: NSArray? = OneRoster.sharedInstance.fetchedResultsController()!.sections
+        if section < sections!.count {
+            let sectionInfo: AnyObject = sections![section]
+            let tmpSection: Int = Int(sectionInfo.name)!
+            
+            switch (tmpSection) {
+            case 0 :
+                return "Online"
+                
+            case 1 :
+                return "Away"
+                
+            default :
+                return "Offline"
+                
+            }
+        }
         
-		let cell: TableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as? TableViewCell
-
-		let user = OneRoster.userFromRosterAtIndexPath(indexPath: indexPath)
+        return ""
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        _ = OneRoster.userFromRosterAtIndexPath(indexPath: indexPath)
         
-		print("Contact list controller \(user)")
+        delegate?.didSelectContact(OneRoster.userFromRosterAtIndexPath(indexPath: indexPath))
+        close(self)
+        
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: TableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as? TableViewCell
+        
+        let user = OneRoster.userFromRosterAtIndexPath(indexPath: indexPath)
+        
+        print("Contact list controller \(user)")
         cell!.userName!.text = user.displayName
         
         cell!.statusLabel!.layer.cornerRadius = cell!.statusLabel!.frame.size.width/2;
@@ -145,77 +141,46 @@ class ContactListTableViewController: UITableViewController, OneRosterDelegate, 
         default:
             cell!.statusLabel!.backgroundColor = UIColor.whiteColor()
         }
-		
-		if user.unreadMessages.intValue > 0 {
-			cell!.backgroundColor = .orangeColor()
-		} else {
-			cell!.backgroundColor = .whiteColor()
-		}
-		
-		OneChat.sharedInstance.configurePhotoForCell(cell!, user: user)
+        
+        if user.unreadMessages.intValue > 0 {
+            cell!.backgroundColor = .orangeColor()
+        } else {
+            cell!.backgroundColor = .whiteColor()
+        }
+        
+        OneChat.sharedInstance.configurePhotoForCell(cell!, user: user)
         cell!.userImage!.image = cell!.imageView?.image
         cell!.imageView!.image = nil
         cell!.userImage!.layer.cornerRadius = cell!.userImage!.frame.size.width/2
         cell!.userImage!.clipsToBounds = true
         cell!.userImage!.contentMode = .ScaleAspectFill
         
-		return cell!;
-	}
-	
-	// Mark: Segue support
-//	
-//	override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-//		if segue?.identifier != "one.to.settings" {
-//			if let controller: ChatViewController? = segue?.destinationViewController as? ChatViewController {
-//				if let cell: UITableViewCell? = sender as? UITableViewCell {
-//					let user = OneRoster.userFromRosterAtIndexPath(indexPath: tableView.indexPathForCell(cell!)!)
-//					controller!.recipient = user
-//				}
-//			}
-//		}
-//	}K
+        return cell!;
+    }
     
-	
-    func didSelectContact(recipient: XMPPUserCoreDataStorageObject) {
-        func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-            if segue?.identifier != "chatToChats" {
-                if let controller: ChatViewController? = segue?.destinationViewController as? ChatViewController {
-                    if let cell: UITableViewCell? = sender as? UITableViewCell {
-                        let user = OneRoster.userFromRosterAtIndexPath(indexPath: tableView.indexPathForCell(cell!)!)
-                        controller!.title = user.displayName
-                        controller!.recipient = user
-                    }
+    // Mark: Segue support
+    
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        if segue?.identifier != "One.HomeToSetting" {
+            if let controller: ChatViewController? = segue?.destinationViewController as? ChatViewController {
+                if let cell: UITableViewCell? = sender as? UITableViewCell {
+                    let user = OneRoster.userFromRosterAtIndexPath(indexPath: tableView.indexPathForCell(cell!)!)
+                    controller!.recipient = user
                 }
             }
         }
     }
     
+    // Mark: IBAction
     
-
-        
-//        self.recipient = recipient
-//        if userDetails == nil {
-//            navigationItem.title = recipient.displayName
-//        }
-//        
-//        if !OneChats.knownUserForJid(jidStr: recipient.jidStr) {
-//            OneChats.addUserToChatList(jidStr: recipient.jidStr)
-//        } else {
-//            messages = OneMessage.sharedInstance.loadArchivedMessagesFrom(jid: recipient.jidStr)
-//            finishReceivingMessageAnimated(true)
-//        }
-
+    @IBAction func close(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
     
-	// Mark: IBAction
-	
-//	@IBAction func close(sender: AnyObject) {
-//		self.dismissViewControllerAnimated(true, completion: nil)
-//	}
-	
-	// Mark: Memory Management
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+    // Mark: Memory Management
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
--- Example/xmpp-messenger-ios/OpenChatsTableViewController.swift
+++ Example/xmpp-messenger-ios/OpenChatsTableViewController.swift
@@ -11,75 +11,73 @@ import xmpp_messenger_ios
 import XMPPFramework
 
 
-class OpenChatsTableViewController: UITableViewController, OneRosterDelegate, OneMessageDelegate {
-	
-	var chatList = NSArray()
-    var msg : AnyObject
+class OpenChatsTableViewController: UITableViewController, OneRosterDelegate, UITabBarDelegate {
     
-	// Mark: Life Cycle
-	
-	override func viewWillAppear(animated: Bool) {
-		super.viewWillAppear(animated)
-		
+    var chatList = NSArray()
+    //    var msg : String?
+    
+    // Mark: Life Cycle
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
         
-//		OneRoster.sharedInstance.delegate = self
-        OneMessage.sharedInstance.delegate = self
         
-		OneChat.sharedInstance.connect(username: kXMPP.myJID, password: kXMPP.myPassword) { (stream, error) -> Void in
+        OneRoster.sharedInstance.delegate = self
+        OneChat.sharedInstance.connect(username: kXMPP.myJID, password: kXMPP.myPassword) { (stream, error) -> Void in
             if let _ = error {
                 self.performSegueWithIdentifier("chatsToSignIn", sender: self)
             } else {
                 //set up online UI
-			}
-		}
-		
-//		self.tableView.rowHeight = 50
-	}
-	
-	override func viewWillDisappear(animated: Bool) {
-		super.viewWillDisappear(animated)
-		
-		OneRoster.sharedInstance.delegate = nil
-	}
-	
-	// Mark: OneRoster Delegates
-	
-	func oneRosterContentChanged(controller: NSFetchedResultsController) {
-		//Will reload the tableView to reflet roster's changes
-		tableView.reloadData()
-	}
-	
-	// Mark: UITableView Datasources
-	
-	override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return OneChats.getChatsList().count
-	}
-	
-	override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-		//let sections: NSArray? = OneRoster.sharedInstance.fetchedResultsController()!.sections
-		return 1//sections
-	}
-	
-	// Mark: UITableView Delegates
-	
-	override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-		return 0.01
-	}
-	
-	override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-		return UIView()
-	}
-	
-	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-		let cell: TableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as? TableViewCell
-		let user = OneChats.getChatsList().objectAtIndex(indexPath.row) as! XMPPUserCoreDataStorageObject
-		
+            }
+        }
+        
+        //		self.tableView.rowHeight = 50
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        OneRoster.sharedInstance.delegate = nil
+    }
+    
+    // Mark: OneRoster Delegates
+    
+    func oneRosterContentChanged(controller: NSFetchedResultsController) {
+        //Will reload the tableView to reflet roster's changes
+        tableView.reloadData()
+    }
+    
+    // Mark: UITableView Datasources
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return OneChats.getChatsList().count
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        //let sections: NSArray? = OneRoster.sharedInstance.fetchedResultsController()!.sections
+        return 1//sections
+    }
+    
+    // Mark: UITableView Delegates
+    
+    override func tableView(tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01
+    }
+    
+    override func tableView(tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: TableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as? TableViewCell
+        let user = OneChats.getChatsList().objectAtIndex(indexPath.row) as! XMPPUserCoreDataStorageObject
+        
         print("Chat list controller + \(user)")
         
-		cell!.userName!.text = user.displayName
+        cell!.userName!.text = user.displayName
         
-		OneChat.sharedInstance.configurePhotoForCell(cell!, user: user)
+        OneChat.sharedInstance.configurePhotoForCell(cell!, user: user)
         cell!.userImage!.image = cell!.imageView?.image
         cell!.imageView!.image = nil
         cell!.userImage!.layer.cornerRadius = cell!.userImage!.frame.size.width/2
@@ -88,7 +86,7 @@ class OpenChatsTableViewController: UITableViewController, OneRosterDelegate, On
         
         cell!.unreadMessages!.layer.cornerRadius = cell!.unreadMessages!.frame.size.width/2;
         cell!.unreadMessages!.clipsToBounds = true
-     
+        
         
         switch user.unreadMessages {
         case 0 :
@@ -109,70 +107,69 @@ class OpenChatsTableViewController: UITableViewController, OneRosterDelegate, On
             cell!.statusLabel!.backgroundColor = UIColor.whiteColor()
         }
         
-		
-		return cell!
-	}
+        
+        return cell!
+    }
     
     // Mark: Chat message Delegates
     
-    func oneStream(sender: XMPPStream, didReceiveMessage message: XMPPMessage, from user: XMPPUserCoreDataStorageObject) {
-         msg = (message.elementForName("body")?.stringValue())!
-        
-        
+    //    func oneStream(sender: XMPPStream, didReceiveMessage message: XMPPMessage, from user: XMPPUserCoreDataStorageObject) {
+    //         msg = (message.elementForName("body")?.stringValue())!
+    //
+    //    }
+    
+    
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            let refreshAlert = UIAlertController(title: "", message: "Are you sure you want to clear the entire message history? \n This cannot be undone.", preferredStyle: UIAlertControllerStyle.ActionSheet)
+            
+            refreshAlert.addAction(UIAlertAction(title: "Clear message history", style: .Destructive, handler: { (action: UIAlertAction!) in
+                OneChats.removeUserAtIndexPath(indexPath)
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
+            }))
+            
+            refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { (action: UIAlertAction!) in
+                
+            }))
+            
+            presentViewController(refreshAlert, animated: true, completion: nil)
+        }
+    }
+    
+    // Mark: Segue support
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        if identifier == "chatsToAddFriend" {
+            if !OneChat.sharedInstance.isConnected() {
+                let alert = UIAlertController(title: "Attention", message: "You have to be connected add a friend", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
+                
+                self.presentViewController(alert, animated: true, completion: nil)
+                
+                return false
+            }
+        }
+        return true
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
+        if segue?.identifier == "chats.to.chat" {
+            if let controller = segue?.destinationViewController as? ChatViewController {
+                if let cell: UITableViewCell? = sender as? UITableViewCell {
+                    let user = OneChats.getChatsList().objectAtIndex(tableView.indexPathForCell(cell!)!.row) as! XMPPUserCoreDataStorageObject
+                    controller.recipient = user
+                }
+            }
+        }
     }
 
     
-	
-	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-		if editingStyle == UITableViewCellEditingStyle.Delete {
-			let refreshAlert = UIAlertController(title: "", message: "Are you sure you want to clear the entire message history? \n This cannot be undone.", preferredStyle: UIAlertControllerStyle.ActionSheet)
-            
-            		refreshAlert.addAction(UIAlertAction(title: "Clear message history", style: .Destructive, handler: { (action: UIAlertAction!) in
-                		OneChats.removeUserAtIndexPath(indexPath)
-                		tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Left)
-            		}))
-            
-            		refreshAlert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: { (action: UIAlertAction!) in
-
-            		}))
-            
-            		presentViewController(refreshAlert, animated: true, completion: nil)
-		}
-	}
-	
-	// Mark: Segue support
-	
-	override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
-		if identifier == "chatsToAdd" {
-			if !OneChat.sharedInstance.isConnected() {
-				let alert = UIAlertController(title: "Attention", message: "You have to be connected to add a friend", preferredStyle: UIAlertControllerStyle.Alert)
-				alert.addAction(UIAlertAction(title: "Ok", style: UIAlertActionStyle.Default, handler: nil))
-				
-				self.presentViewController(alert, animated: true, completion: nil)
-				
-				return false
-			}
-		}
-		return true
-	}
-	
-	override func prepareForSegue(segue: UIStoryboardSegue?, sender: AnyObject?) {
-		if segue?.identifier == "chatsToChat" {
-			if let controller = segue?.destinationViewController as? ChatViewController {
-				if let cell: UITableViewCell? = sender as? UITableViewCell {
-					let user = OneChats.getChatsList().objectAtIndex(tableView.indexPathForCell(cell!)!.row) as! XMPPUserCoreDataStorageObject
-					controller.recipient = user
-				}
-			}
-		}
-	}
+    // Mark: Memory Management
     
-	
-	// Mark: Memory Management
-	
-	override func didReceiveMemoryWarning() {
-		
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
+    override func didReceiveMemoryWarning() {
+        
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
